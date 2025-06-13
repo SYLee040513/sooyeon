a72230d5450de364ffc99506e1e8e030bf5d9afc
SYLee040513 <sooyoun040513@gmail.com>
2025-06-13 22:03:42 +0900

    Add files via upload
    Item 구조체에 value_size 추가 적용
    
    enqueue() 시 value 복사 시 사이즈 지정 구현
    
    
    동기화(mutex) 기반의 thread-safe priority queue 구현 
    
    init(), release(), enqueue(), dequeue(), range()  구현
    
    nalloc(), nfree(), nclone() 구현

---
+++ qtype.h
@@ -0,0 +1,46 @@
+﻿// ConsoleApplication4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+#include <mutex>
+#include <cstring>
+int main() {
+
+}
+
+#pragma once
+
+#include <cstdint>
+
+typedef uint32_t Key;
+typedef uint8_t* Value;
+
+typedef struct {
+    Key key;
+    Value value;
+    int value_size; // 메모리 복사에 사용됨
+} Item;
+
+typedef struct node_t {
+    Item item;
+    struct node_t* next;
+} Node;
+
+#ifdef __cplusplus
+#include <mutex>
+typedef struct Queue {
+    Node* head, * tail;
+    std::mutex lock; // C++ 환경에서만 사용
+} Queue;
+#else
+typedef struct {
+    Node* head, * tail;
+} Queue;
+#endif
+
+typedef struct {
+    bool success;
+    Item item;
+} Reply;
